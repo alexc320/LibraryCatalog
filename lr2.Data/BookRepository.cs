4e3b142ca2479651cea6c104b9be4ee919eef647
alex567-SA <krutovworky@gmail.com>
2026-09-09 22:08:19 +0300

    Добавить модель и хранилище данных

---
+++ lr2.Data/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lr2.Data
+{
+    public class BookRepository
+    {
+        private readonly List<Book> _books = new()
+        {
+            new Book { Id = 1, Title = "Война и мир", Year = 1869 },
+            new Book { Id = 2, Title = "Преступление и наказание", Year = 1866 },
+            new Book { Id = 3, Title = "Мастер и Маргарита", Year = 2006 }
+        };
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+    }
+}
