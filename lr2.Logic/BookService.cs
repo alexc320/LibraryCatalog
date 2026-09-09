93993821bf1fa04c74059209b2248eb8acdf699f
alex567-SA <krutovworky@gmail.com>
2026-09-09 22:08:59 +0300

    Добавить слой логики и вывод результата

---
+++ lr2.Logic/BookService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lr2.Data;
+
+namespace lr2.Logic
+{
+    public class BookService
+    {
+        private readonly BookRepository _repository = new();
+
+        public List<Book> GetImportant()
+        {
+            return _repository.GetAll()
+                .Where(b => b.Year < 2000)
+                .ToList();
+        }
+    }
+}
